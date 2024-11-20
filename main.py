9f6913642f866147969b8c0477f0814ef9491970
Andrés Yepes <102176786+Broncos225@users.noreply.github.com>
2024-11-19 21:05:17 -0500

    v1.2

--- main.py
+++ main.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/El Leon de Nemea')
+def Leon():
+    return render_template('Leon.html')
+
+@app.route('/La Hidra de Lerna')
+def Hidra():
+    return render_template('Hidra.html')
+
+@app.route('/El Ciervo de Cerinea')
+def Ciervo():
+    return render_template('Ciervo.html')
+
+@app.route('/El jabali de Erimanto')
+def Jabali():
+    return render_template('Jabali.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
