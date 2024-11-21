1792f6cb17a15b97520e0b13e99cce35fe83a328
Andrés Yepes <102176786+Broncos225@users.noreply.github.com>
2024-11-20 20:04:54 -0500

    v1.3

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import sqlite3
 
 app = Flask(__name__)
+
+# Función para obtener la conexión a la base de datos
+def get_db_connection():
+    conn = sqlite3.connect('db/database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/data')
+def get_data():
+    conn = get_db_connection()
+    data = conn.execute('SELECT date, value FROM progress').fetchall()
+    conn.close()
+    return jsonify([dict(row) for row in data])
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/data2')
+def data():
+    return render_template('data.html')
 
 @app.route('/El Leon de Nemea')
 def Leon():
