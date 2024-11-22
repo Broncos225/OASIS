7ac6493893485c8385f56ad79237df1d75877a0b
Andrés Yepes <102176786+Broncos225@users.noreply.github.com>
2024-11-21 21:27:52 -0500

    v1.4

--- main.py
+++ main.py
@@ -3,39 +3,40 @@ import sqlite3
 
 app = Flask(__name__)
 
-# Función para obtener la conexión a la base de datos
+# CONEXION A LA BASE DE DATOS
 def get_db_connection():
     conn = sqlite3.connect('db/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/data')
-def get_data():
-    conn = get_db_connection()
-    data = conn.execute('SELECT date, value FROM progress').fetchall()
-    conn.close()
-    return jsonify([dict(row) for row in data])
 
+
+# INICIO
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/data2')
-def data():
-    return render_template('data.html')
+# BITACORA
+@app.route('/Bitacora')
+def Bitacora():
+    return render_template('Bitacora.html')
 
+# EL LEON DE NEMEA
 @app.route('/El Leon de Nemea')
 def Leon():
     return render_template('Leon.html')
 
+# LA HIDRA DE LERNA
 @app.route('/La Hidra de Lerna')
 def Hidra():
     return render_template('Hidra.html')
 
+# EL CIERVO DE CERINEA
 @app.route('/El Ciervo de Cerinea')
 def Ciervo():
     return render_template('Ciervo.html')
 
+# EL JABALI DE ERIMANTO
 @app.route('/El jabali de Erimanto')
 def Jabali():
     return render_template('Jabali.html')
